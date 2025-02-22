b4b90a80e8178f6d12d6590d75084a064c2134c2
UZMA FAROOQUI <uzmafarooqui@gmail.com>
2025-02-21 21:58:40 -0600

    Project4Stage3

--- scripts/index.js
+++ scripts/index.js
@@ -25,4 +25,19 @@ const initalCards = [
   },
 ];
 
-console.log(initalCards);
+const profileEditButton = document.querySelector(".profile__edit-btn");
+
+const editModal = document.querySelector("#edit-modal");
+const editModalCloseBtn = editModal.querySelector(".modal__close-btn");
+
+function openModal() {
+  editModal.classList.add("modal__opened");
+}
+
+function closeModal() {
+  editModal.classList.remove("modal__opened");
+}
+
+profileEditButton.addEventListener("click", openModal);
+
+editModalCloseBtn.addEventListener("click", closeModal);
